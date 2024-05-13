bbc2a0e400c16faac5b2896b87a5b9336020e198
Mitchell <mitchell.b.chelin@gmail.com>
2024-05-13 13:07:55 +1000

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -10,7 +10,7 @@
  * Plugin Name:	   	Types
  * Plugin URI:		https://github.com/mitchell-b-chelin/Types/blob/main/LICENSE
  * Description:	   	A developer friendly Custom Post Type registration method.
- * Version:		   	1.0.7
+ * Version:		   	1.0.8
  * Author:			Mitchell-Blair Chelin
  * Author URI:		https://github.com/mitchell-b-chelin/
  * License:		   	MIT
